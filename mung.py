be9864d7abf3ed2f9b94dd778f739c2d38df16a6
Kevin Marks <kevinmarks@gmail.com>
2016-07-16 00:47:19 -0700

    process into /previous

--- mung.py
+++ mung.py
@@ -2,4 +2,4 @@ import makerelative
 
 makerelative.processfiles('raw dumps/www.lifewithalacrity.com','../scratchspace','http://www.lifewithalacrity.com')
 ## then flatten out the legacy domain too
-makerelative.processfiles('../scratchspace','../lifewithalacrity.github.io','http://lifewithalacrity.blogs.com')
+makerelative.processfiles('../scratchspace','../lifewithalacrity.github.io/previous','http://lifewithalacrity.blogs.com')
